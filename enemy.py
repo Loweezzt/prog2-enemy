49a9483fc7ea25eced1be77e9f543df608f3efc7
Loweezzt <lowe.vesterberg@elev.ga.ntig.se>
2025-09-26 11:06:13 +0200

    funkar inte riktigt men försöker lista ut vad felet är.

--- enemy.py
+++ enemy.py
@@ -1,8 +1,18 @@
 class Enemy:
-    def __init__(self):
-        self.name = "Goblin"
-        self.health = 100
-        self.attack_power = 15
+    def __init__(self, health: int, attack_power: int, name: str):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
 
-    def print_status(self):
-        print(f"Enemy: {self.name}, Health: {self.health}, Attack Power: {self.attack_power}")
+    def attack(self):
+        print(f"{self.name} attackerar med {self.attack_power} skada")
+
+    def take_damage(self, damage: int):
+        self.health -= damage
+        print(f"{self.name} tar {damage} skada och har {self.health} HP")
+
+        def print_status(self):
+          print(f"{self.name} har {self.health} HP")
+
+    Goblin = Enemy(15, 100, "Goblin")
+    Goblin.print_status()
