1924be489757dad1f048319ce4e575edff84d43b
Loweezzt <lowe.vesterberg@elev.ga.ntig.se>
2025-10-03 11:01:35 +0200

    Jag tycker det är svårt med att förstå exakt vad som skrivs när man "skriver av" dig. Skulle liksom behöva en kvart eller tjugio minuter när du hjälper mig förstå vad vi redan skrivigt. Så det blir lätt senare att gå vidare.

--- enemy.py
+++ enemy.py
@@ -11,8 +11,27 @@ class Enemy:
         self.health -= damage
         print(f"{self.name} tar {damage} skada och har {self.health} HP")
 
+    def print_status(self):
+        print(f"{self.name} har {self.health} HP")
+
+Goblin = Enemy(15, 100, "Goblin")
+Goblin.print_status()
+ 
+class Enemy2:
+    def __init__(self, health: int, attack_power: int, name: str):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+    def attack(self):
+        print(f"{self.name} attackerar med {self.attack_power} skada")
+
+    def take_damage(self, damage: int):
+        self.health -= damage
+        print(f"{self.name} tar {damage} skada och har {self.health} HP")
+
         def print_status(self):
           print(f"{self.name} har {self.health} HP")
 
-    Goblin = Enemy(15, 100, "Goblin")
-    Goblin.print_status()
+Goblin = Enemy2(15, 100, "Skeleton")
+Goblin.print_status()
